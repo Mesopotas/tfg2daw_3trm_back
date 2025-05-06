6bcf3a63a4ca458720e727736fde83b3f4c7a46e
PruebaGit <n.abuin2005@gmail.com>
2025-05-06 22:00:09 +0200

    Creacion repository de caracteristicaSala con Entity Framework

---
+++ Repository/CaracteristicasSalaRepository.cs
@@ -0,0 +1,55 @@
+using Microsoft.Data.SqlClient;
+using Models;
+using CoWorking.DTO;
+using CoWorking.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CoWorking.Repositories
+{
+    public class CaracteristicasSalaRepository : ICaracteristicasSalaRepository
+    {
+        private readonly CoworkingDBContext _context;
+
+
+        public CaracteristicasSalaRepository(CoworkingDBContext context) // referencia al data.CoworkingDBContext.cs en lugar de cadena de conexión, el EF hará las sentencias sin ponerlas explicitamente
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<CaracteristicasSala>> GetAllAsync()
+        {
+            return await _context.CaracteristicasSala.ToListAsync(); // el ToListAsync hará una sentencia que devuelva todos los datos de la tabla Usuarios, equivalente a SELECT * FROM Usuarios
+        }
+
+        public async Task<CaracteristicasSala> GetByIdAsync(int id)
+        {
+            return await _context.CaracteristicasSala.FirstOrDefaultAsync(caracteristicaSala => caracteristicaSala.IdCaracteristica == id); // funcion flecha, usuario recoge todos los usuarios quer cumple que IdUsuario == id
+        }
+        public async Task AddAsync(CaracteristicasSala caracteristicaSala)
+        {
+
+            await _context.CaracteristicasSala.AddAsync(caracteristicaSala); // AddAsync es metodo propio de EF, no hace el insert en si, solo lo prepara
+            await _context.SaveChangesAsync(); // otro metodo de EF, esto si hace el insert con los datos del add, ambos son imprescindibles para el insert
+        }
+
+
+        public async Task UpdateAsync(CaracteristicasSala caracteristicaSala)
+        {
+            _context.CaracteristicasSala.Update(caracteristicaSala); // igual que el add pero haciendo un update
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var caracteristicaSala = await GetByIdAsync(id); // primero busca el id del usuario
+            if (caracteristicaSala != null)
+            {// si existe, pasa a ejecutar
+
+                _context.CaracteristicasSala.Remove(caracteristicaSala); // metodo de EF para eliminar registros (los prepara para eliminacion)
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
