93525873cb4320abad216b832f3a8d274357d7dd
nabuin <nabuin@hiberus.com>
2025-05-07 08:45:33 +0200

    Implementacion de Interface en caracteristicaSala

---
+++ Service/CaracteristicasSalaService.cs
@@ -0,0 +1,53 @@
+using Microsoft.Data.SqlClient;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoWorking.Repositories;
+using CoWorking.DTO;
+using CoWorking.Service;
+using System.Security.Claims;
+
+namespace CoWorking.Service
+{
+    public class CaracteristicasSala : ICaracteristicasSalaService
+    {
+        private readonly ICaracteristicasSalaRepository _caracteristicasSalaRepository;
+
+        public CaracteristicasSalaService(ICaracteristicasSalaRepository caracteristicasSalaRepository)
+        {
+            _caracteristicasSalaRepository = caracteristicasSalaRepository;
+        }
+
+        public async Task<List<CaracteristicasSala>> GetAllAsync()
+        {
+            return await _caracteristicasSalaRepository.GetAllAsync();
+        }
+
+        public async Task<CaracteristicasSala?> GetByIdAsync(int id)
+        {
+            return await _caracteristicasSalaRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(CaracteristicasSala caracteristicaSala)
+        {
+            await _caracteristicasSalaRepository.AddAsync(caracteristicaSala);
+        }
+
+        public async Task UpdateAsync(CaracteristicasSala caracteristicaSala)
+        {
+            await _caracteristicasSalaRepository.UpdateAsync(caracteristicaSala);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var caracteristicaSala = await _caracteristicasSalaRepository.GetByIdAsync(id);
+            if (caracteristicaSala == null)
+            {
+                //return NotFound();
+            }
+            await _caracteristicasSalaRepository.DeleteAsync(id);
+            //return NoContent();
+        }
+    }
+}
